f90fd1cb1b8964f5d5daec6c5407d040c1ad9875
Mitch <liquidvisual@gmail.com>
2018-01-14 22:32:04 +1100

    Built default from commit 33f8f40 on branch master

---
+++ tutorials/posts.js
@@ -0,0 +1,71 @@
+// const
+
+var posts = [
+  
+    {
+      id: "1",
+      date: "2015-02-03 00:00:00 +1100",
+      date_formatted: "03 February 2015",
+      title: "How to Correctly Mark Up Your Company&#39;s Logo",
+      url: "/tutorials/how-to-correctly-mark-up-your-companys-logo/",
+      hero: "/assets/img/content/hero-home-9.jpg",
+      // categories: "Development",
+      excerpt: "My lucky stars ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore mag...",
+    },
+  
+    {
+      id: "2",
+      date: "2015-02-03 00:00:00 +1100",
+      date_formatted: "03 February 2015",
+      title: "Embed the Latest YouTube Video with a Channel ID",
+      url: "/tutorials/embed-the-latest-youtube-video-with-a-channel-id/",
+      hero: "/assets/img/content/hero/youtube.jpg",
+      // categories: "Development",
+      excerpt: "Because responsive design is hard, first and foremost. These offshore businesses are dealing with hundreds of clients and t...",
+    },
+  
+    {
+      id: "3",
+      date: "2015-02-03 00:00:00 +1100",
+      date_formatted: "03 February 2015",
+      title: "Embed a Google Map into your Website Without an API Key",
+      url: "/tutorials/embed-a-google-map-into-your-website-without-an-api-key/",
+      hero: "/assets/img/content/hero/hero-google-maps-1.png",
+      // categories: "Development",
+      excerpt: "Because responsive design is hard, first and foremost. These offshore businesses are dealing with hundreds of clients and t...",
+    },
+  
+    {
+      id: "4",
+      date: "2015-02-03 00:00:00 +1100",
+      date_formatted: "03 February 2015",
+      title: "Create a Sprite Animation with CSS",
+      url: "/tutorials/create-a-sprite-animation-with-css/",
+      hero: "/assets/img/content/hero-home-9.jpg",
+      // categories: "CSS",
+      excerpt: "Because responsive design is hard, first and foremost. These offshore businesses are dealing with hundreds of clients and t...",
+    },
+  
+    {
+      id: "5",
+      date: "2015-02-03 00:00:00 +1100",
+      date_formatted: "03 February 2015",
+      title: "Embed Tagged Instagram Posts into your Website",
+      url: "/tutorials/embed-tagged-instagram-posts-with-the-new-api-changes/",
+      hero: "/assets/img/content/hero/hero-home-8.jpg",
+      // categories: "Development",
+      excerpt: "Because responsive design is hard, first and foremost. These offshore businesses are dealing with hundreds of clients and t...",
+    },
+  
+    {
+      id: "6",
+      date: "2015-02-03 00:00:00 +1100",
+      date_formatted: "03 February 2015",
+      title: "Install NPM and Jekyll Locally Without Sudo",
+      url: "/tutorials/install-npm-and-jekyll-locally-on-mac-without-sudo/",
+      hero: "/assets/img/content/hero/hero-pinky.png",
+      // categories: "Development",
+      excerpt: "Because responsive design is hard, first and foremost. These offshore businesses are dealing with hundreds of clients and t...",
+    }
+  
+];
